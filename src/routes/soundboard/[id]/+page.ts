9c7558f1b67f1baf6ce5284105693f50853d7abc
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-27 11:23:19 +0200

    error handling on soundboard[id] route loading

--- src/routes/soundboard/[id]/+page.ts
+++ src/routes/soundboard/[id]/+page.ts
@@ -1,11 +1,14 @@
 import type { PageLoad } from './$types';
 import type { SoundBoardItem } from '$lib/types';
 import { baseURL } from '$lib/backend';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params, fetch }) => {
 	const res = await fetch(`${baseURL}/items`);
-	const items = await res.json();
-	const item: SoundBoardItem = items.find((i: SoundBoardItem) => i.id === +params.id);
+	const items: SoundBoardItem[] = await res.json();
+	const item = items.find((i) => i.id === +params.id);
+
+	if (!item) throw error(404);
 
 	return {
 		soundBoardItem: item
