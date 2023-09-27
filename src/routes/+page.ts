a66885438309f44e89890ad9652194626d707529
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-27 11:39:03 +0200

    using env variable to define base backend url

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,9 @@
 import type { PageLoad } from './$types';
 import type { SoundBoardItem } from '$lib/types';
-// import { PUBLIC_VITE_BACKEND_URL } from '$env/static/public';
-import { baseURL } from '$lib/backend';
+import { PUBLIC_BASE_URL } from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch(`${baseURL}/items`);
+	const res = await fetch(`${PUBLIC_BASE_URL}/items`);
 	const items: SoundBoardItem[] = await res.json();
 
 	return {
--- src/routes/soundboard/[id]/+page.ts
+++ src/routes/soundboard/[id]/+page.ts
@@ -1,10 +1,10 @@
 import type { PageLoad } from './$types';
 import type { SoundBoardItem } from '$lib/types';
-import { baseURL } from '$lib/backend';
+import { PUBLIC_BASE_URL } from '$env/static/public';
 import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-	const res = await fetch(`${baseURL}/items`);
+	const res = await fetch(`${PUBLIC_BASE_URL}/items`);
 	const items: SoundBoardItem[] = await res.json();
 	const item = items.find((i) => i.id === +params.id);
 
