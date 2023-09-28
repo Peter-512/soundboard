73d09c09fd3a3c49cf0a31aa5ac1f51f63104afd
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-28 16:02:08 +0200

    defined zod schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,14 @@
-export interface SoundBoardItem {
-	id: number;
-	imgSrc: string;
-	soundSrc: string;
-	alt: string;
-	name: string;
-}
+import { z } from 'zod';
+
+export const soundBoardItemSchema = z.object({
+	id: z.number().gt(0),
+	imgSrc: z.string().url(),
+	soundSrc: z.string().url(),
+	alt: z.string().nonempty(),
+	name: z.string().nonempty()
+});
+
+export type SoundBoardItem = z.infer<typeof soundBoardItemSchema>;
 
 export interface Category {
 	name: string;
