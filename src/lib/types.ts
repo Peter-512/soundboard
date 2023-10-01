cc6ae42ba8be1016f5199a90e316bb869eb2dc3f
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-10-01 20:15:49 +0200

    api for deploy to vercel

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const soundBoardItemSchema = z.object({
-	imgSrc: z.string().url(),
-	soundSrc: z.string().url(),
+	imgSrc: z
+		.string()
+		.url()
+		.regex(/\.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico|heic|heif)$/, 'Link should link to an image'),
+	soundSrc: z.string().url().endsWith('.mp3', 'Only MP3 files are supported'),
 	alt: z.string().nonempty(),
 	name: z.string().min(5),
 	category: z.enum(['dragons', 'werewolves'])
